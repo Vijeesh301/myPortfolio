16576f4af63e29f6d759ec2d1dae6ad98b5ed51b
vijeesh <vichus301@gmail.com>
2026-08-05 02:31:16 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>test</div>
+      <div className="min-h-screen bg-linear-to-b from-[#25011e] to-[#060003]">
+        Content
+      </div>
     </>
   );
 }
